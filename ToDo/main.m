71a499e17757e556c415ece4b82f17f4db1c1af8
drenganathan <drenganathan@gmail.com>
2013-08-12 22:16:54 +0530

    Updates

---
+++ ToDo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDo
+//
+//  Created by Renganathan D on 8/11/13.
+//  Copyright (c) 2013 Renganathan D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ToDoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ToDoAppDelegate class]));
+    }
+}
